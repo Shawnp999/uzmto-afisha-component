a075ea8fe4fcad5df90dbe0516e45af5f8339d95
Shawn Pantzlaff <2001shawnp@gmail.com>
2025-06-24 10:01:59 +0500

    init commit afisha uzmto component

---
+++ src/webparts/afishaComponent/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface IAfishaComponentWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'AfishaComponentWebPartStrings' {
+  const strings: IAfishaComponentWebPartStrings;
+  export = strings;
+}
